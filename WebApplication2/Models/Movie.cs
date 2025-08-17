70754f80b95b76e8fb25deeab8034d2c4ac35206
CambridgeZ <1376734195@qq.com>
2025-08-17 22:07:13 +0800

    add some information on movies

--- WebApplication2/Models/Movie.cs
+++ WebApplication2/Models/Movie.cs
@@ -5,11 +5,22 @@ namespace WebApplication2.Models;
 public class Movie
 {
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength =3)]
+    [Required]
     public string? Title { get; set; }
-    
+
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
     public string? Genre { get; set; }
+    
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
     public decimal Price { get; set; }
 }
